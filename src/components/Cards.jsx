af66f7435ca72f2e686c932ebf64c493e4efbc26
Nicolás Valor <nicoo.valor@gmail.com>
2023-01-09 10:46:14 -0300

    Comenzando la app de rick y morty

---
+++ src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+export default function Cards(props) {
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map((char) => (
+        <Card
+          name={char.name}
+          species={char.species}
+          gender={char.gender}
+          image={char.image}
+          onClose={() => window.alert("Emulamos que se cierra la card")}
+        />
+      ))}
+    </div>
+  );
+}
